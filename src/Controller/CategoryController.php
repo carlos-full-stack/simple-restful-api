3869b81656f699bfef545689e124307d12d892b8
Newbiecodeweaver <carlosgilino@gmail.com>
2023-03-22 00:29:54 +0100

    define newCategory, showCategory, editCategory and deleteCategory

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,7 +7,9 @@ use App\Service\DataGenerator;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
 * @Route("/api", name="api_")
@@ -20,7 +22,7 @@ class CategoryController extends AbstractController
 * @Route("/categories", name="category_index", methods={"GET"})
 */
 
-public function indexProduct(ManagerRegistry $doctrine, DataGenerator $dg): JsonResponse
+public function indexCategory(ManagerRegistry $doctrine, DataGenerator $dg): JsonResponse
 {
     $categories = $doctrine
         ->getRepository(Category::class)
@@ -30,5 +32,97 @@ public function indexProduct(ManagerRegistry $doctrine, DataGenerator $dg): Json
 
     return $this->json($dg->generateCategoryData($categories));
 }
+
+/**
+* @Route("/category/new", name="category_new", methods={"POST"})
+*/
+
+public function newCategory(ManagerRegistry $doctrine, Request $request, ValidatorInterface $validator)
+{
+
+    $entityManager = $doctrine->getManager();
+    $category = new Category();
+
+    $category->setName($request->request->get('name'));
+    $category->setDescription($request->request->get('description'));
+
+    $errors = $validator->validate($category);
+
+    if (count($errors) > 0) {
+  
+    $errorsString = (string) $errors;
+    return new JsonResponse($errorsString);
+
+    }
+
+
+    $entityManager->persist($category);
+    $entityManager->flush();
+
+    return $this->json('New category has been added with id ' . $category->getId());
+    
+    }
+
+    /**
+    * @Route("/category/{id}", name="category_show", methods={"GET"})
+    */
+    
+    public function showCategory(ManagerRegistry $doctrine, DataGenerator $dg, int $id): JsonResponse
+    {
+
+        $category = $doctrine
+        ->getRepository(Category::class)
+        ->find($id);
+
+        if (!$category) return $this->json('category not found by id ' . $id , 404);
+
+        return $this->json($dg->generatecategoryData($category));
+
+    }
+
+    /**
+    * @Route("/category/{id}", name="category_edit", methods={"PUT"})
+    */
+
+    public function editCategory(ManagerRegistry $doctrine, Request $request, ValidatorInterface $validator, DataGenerator $dg, int $id)
+    {
+        $entityManager = $doctrine->getManager();
+        $category = $entityManager->getRepository(Category::class)->find($id);
+  
+        $category->setName($request->request->get('name'));
+        $category->setDescription($request->request->get('description'));
+
+        $errors = $validator->validate($category);
+
+        if (count($errors) > 0) {
+
+        $errorsString = (string) $errors;
+        return new JsonResponse($errorsString);
+
+        }
+
+        $entityManager->flush();
+  
+        return $this->json($dg->generatecategoryData($category));
+     
+    }
+
+    /**
+    * @Route("/category/{id}", name="category_delete", methods={"DELETE"})
+    */
+
+    public function deleteCategory(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $category = $entityManager->getRepository(category::class)->find($id);
+  
+        if (!$category) return $this->json('category not found by id ' . $id , 404);
+  
+        $entityManager->remove($category);
+        $entityManager->flush();
+  
+        return $this->json('category deleted with id ' . $id);
+    
+    }
 
 }
