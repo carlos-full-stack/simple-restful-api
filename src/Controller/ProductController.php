d9a498b71ba0bb277e844cde7f9b483c4bca08b2
Newbiecodeweaver <carlosgilino@gmail.com>
2023-03-23 17:14:19 +0100

    complete ProductController methods

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Product;
-use App\Service\ProductGenerator;
+use App\Service\DataGenerator;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
 * @Route("/api", name="api_")
@@ -20,54 +23,66 @@ class ProductController extends AbstractController
 /**
 * @Route("/products", name="product_index", methods={"GET"})
 */
-    public function indexProduct(ManagerRegistry $doctrine, ProductGenerator $pg): JsonResponse
+    public function indexProduct(ManagerRegistry $doctrine, DataGenerator $dg)
     {
+
         $products = $doctrine
             ->getRepository(Product::class)
             ->findAll();
 
         if (!$products) return $this->json('There is no products to list', 404);
-  
-        return $this->json($pg->generateProductData($products));
+
+        return $this->json($dg->generateProductData($products));
     }
 
 
 /**
-* @Route("/products/new", name="product_new", methods={"POST"})
+* @Route("/product/new", name="product_new", methods={"POST"})
 */
 
-public function newProduct(ManagerRegistry $doctrine, Request $request): JsonResponse
-{
-    $entityManager = $doctrine->getManager();
-    $product = new Product();
-    $product->setName($request->request->get('name'));
-    $product->setDescription($request->request->get('description'));
-    $product->setWeight($request->request->get('weight'));
-    $product->setIsAvailable($request->request->get('isAvailable'));
-    $product->setQty($request->request->get('qty'));
-    $product->setImage($request->request->get('image'));
+    public function newProduct(ManagerRegistry $doctrine, Request $request, ValidatorInterface $validator)
+    {
+        $entityManager = $doctrine->getManager();
+        $product = new Product();
+
+        $product->setName($request->request->get('name'));
+        $product->setDescription($request->request->get('description'));
+        $product->setWeight($request->request->get('weight'));
+        $product->setIsAvailable($request->request->get('isAvailable'));
+        $product->setQty($request->request->getInt('qty'));
+        $product->setImage($request->request->get('image'));
+        $category = $entityManager->getRepository(Category::class)->find($request->request->get('category'));
+        $product->setCategory($category);
+
+        $errors = $validator->validate($product);
+
+        if (count($errors) > 0) {
+       
+        $errorsString = (string) $errors;
+        return new Response($errorsString);
+
+        }
 
 
-    $entityManager->persist($product);
-    $entityManager->flush();
+        $entityManager->persist($product);
+        $entityManager->flush();
 
-    return $this->json('New product has been added with id ' . $product->getId());
-}
+        return $this->json('New product has been added with id ' . $product->getId());
+    }
 
 
 /**
 * @Route("/product/{id}", name="product_show", methods={"GET"})
 */
-    public function showProduct(ManagerRegistry $doctrine, ProductGenerator $pg, int $id): JsonResponse
+    public function showProduct(ManagerRegistry $doctrine, DataGenerator $dg, int $id): JsonResponse
     {
         $product = $doctrine
         ->getRepository(Product::class)
         ->find($id);
   
         if (!$product) return $this->json('Product not found by id ' . $id , 404);
-    
 
-        return $this->json($pg->generateProductData($product));
+        return $this->json($dg->generateProductData($product));
 
     }
 
@@ -75,22 +90,32 @@ public function newProduct(ManagerRegistry $doctrine, Request $request): JsonRes
 * @Route("/product/{id}", name="product_edit", methods={"PUT"})
 */
 
-    public function editProduct(ManagerRegistry $doctrine, Request $request, ProductGenerator $pg, int $id): JsonResponse
+    public function editProduct(ManagerRegistry $doctrine, Request $request, ValidatorInterface $validator, DataGenerator $dg, int $id)
     {
         $entityManager = $doctrine->getManager();
         $product = $entityManager->getRepository(Product::class)->find($id);
-  
-        if (!$product) return $this->json('Product not found by id ' . $id , 404);
   
         $product->setName($request->request->get('name'));
         $product->setDescription($request->request->get('description'));
         $product->setWeight($request->request->get('weight'));
         $product->setIsAvailable($request->request->get('isAvailable'));
-        $product->setQty($request->request->get('qty'));
+        $product->setQty($request->request->getInt('qty'));
         $product->setImage($request->request->get('image'));
+        $category = $entityManager->getRepository(Category::class)->find($request->request->get('category'));
+        $product->setCategory($category);
+
+        $errors = $validator->validate($product);
+
+        if (count($errors) > 0) {
+
+        $errorsString = (string) $errors;
+        return new Response($errorsString);
+
+        }
+
         $entityManager->flush();
   
-        return $this->json($pg->generateProductData($product));
+        return $this->json($dg->generateProductData($product));
      
     }
 
